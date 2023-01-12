86bfbb6db4ba0951eb42eb3eec744ba3fa8e301a
Jayden Doll <jayjdoll002@gmail.com>
2023-01-12 11:20:58 -0800

    Update Spline packages

--- src/utils/splinePlanet.utils.js
+++ src/utils/splinePlanet.utils.js
@@ -4,7 +4,7 @@ export const planets = {
   venus: "https://prod.spline.design/WuZOqS1iq7jOeQQV/scene.splinecode",
   earth: "https://prod.spline.design/iDOO8WXRef7JxBwM/scene.splinecode",
   moon: "https://prod.spline.design/0QyQy5xBgCa5ttBq/scene.splinecode",
-  mars: "https://prod.spline.design/SpJpq3nrSkpcT6eY/scene.splinecode",
+  mars: "https://prod.spline.design/w6HDWnbh4Oq0qLr0/scene.splinecode",
   jupiter: "https://prod.spline.design/6VnyO2Dr7U1xkL3b/scene.splinecode",
   saturn: "https://prod.spline.design/Fzdg9kD0la2KJYxy/scene.splinecode",
   uranus: "https://prod.spline.design/j5Z-47LaEM-ZjeAI/scene.splinecode",
